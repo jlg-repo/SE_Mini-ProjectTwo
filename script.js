10b86263ed24a7b66edfe91a97ba96076da4d951
jlg-repo <jlgbusiness@proton.me>
2026-02-11 15:10:08 -0500

    Added new logic for cart from Zane

--- script.js
+++ script.js
@@ -25,28 +25,50 @@ function RemoveFromCart(index) {
  console.log(index);
 }
 function DisplayCartItems(){
-
-    
-    console.log("Accesing Cart")
+    const num0 =0;
+        const num1 =0;
+            const num2 =0;
+                const num3 =0;
+      for (var i=0; i <CartArray.length;++i){
+        //Count of each type of item
+        if(CartArray[i] == "0"){
+            //Add to count of item type 0
+            let num0 = num0+1;
+        }
+                if(CartArray[i] == "1"){
+                        //Add to count of item type 1
+                        let num1 = num1+1;
+        }
+                if(CartArray[i] == "2"){
+                        //Add to count of item type 2
+                        let num2 = num2+1;
+        }
+                if(CartArray[i] == "3"){
+                        //Add to count of item type 3
+                        let num3 = num3+1;
+        }
+      }
+      const tempArray = new Array(num0,num1,num2,num3);
     const divContainer = document.getElementById("CartItems");
-    divContainer.innerHTML = "";
-    // TODO: Fix boolean display issue when cart = 0
-    // TODO: Investigate live cart updating
-    // TODO: Create new class for remove button and clear button to change their styling together
-    
-    for (var i=0; i <CartArray.length;++i){
-        console.log(CartArray)
+    for (var i=0; i <tempArray.length;++i){
+        //Displays indexed Item
             const p = document.createElement("p");
-            const bakedNumber= CartArray[i]
-            p.textContent = NameArray[bakedNumber];
+            const bakedNumber= i;
+            p.textContent = NameArray[bakedNumber]+tempArray[i];
             const removeButton = document.createElement("Button");
             removeButton.textContent = "Remove";
             removeButton.onclick  = () => RemoveFromCart(i);
             divContainer.appendChild(p);
             divContainer.appendChild(removeButton);
     }
+    //Clear Button 
     const ClearButton = document.createElement("Button");
     ClearButton.textContent = "Clear Cart";
     ClearButton.onclick = () => ClearCart();
     divContainer.appendChild(ClearButton);
+    // Close Cart Button Replace Later
+    const CloseCart = document.createElement("Button");
+    CloseCart.textContent = "Close Cart";
+    CloseCart.onclick = () => CloseCart();
+    divContainer.appendChild(CloseCart);
 }
